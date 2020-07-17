211b2a4981ba33aee8f2060cdf41ac035eba2171
Frederic Poitevin <frederic.poitevin@gmail.com>
2020-07-17 12:43:40 -0700

    started adding docker build for crystfel

--- dags/reconstruction.py
+++ dags/reconstruction.py
@@ -2,35 +2,20 @@ import os
 from datetime import datetime
 
 from airflow import DAG
+from airflow.models import BaseOperator, SkipMixin
 from airflow.operators.dummy_operator  import DummyOperator
-from airflow.operators.bash_operator   import BashOperator
-from airflow.operators.python_operator import PythonOperator
-from airflow.operators.subdag_operator import SubDagOperator
-#from reconstruction_subdag import consensus_cell_subdag
+from airflow.operators.jid_plugins import JIDOperator
 
+from airflow.exceptions import AirflowException, AirflowSkipException
 
-##### TEMPORARY FILE DEFINITIONS (should not be hard coded...) 
-base_path = '/usr/local/airflow/'
-exp_name = 'cxic0415'
-exp_path = 'dags/data/${exp_name}/'
-fasta_filepath  = 'test.fasta'
-stream_filepath = 'cxic0415.stream'
-mtz_filepath    = 'test.mtz'
-map_filepath    = 'test.map'
-pdb_filepath    = 'test.pdb'
-nb_filepath     = 'test.ipynb'
-nb_publishedpath= 'test.published'
-#####
+import logging
+LOG = logging.getLogger(__name__)
 
-
-class FileSensor( BashOperator ):
+class FileSensor( DummyOperator ):
   ui_color = '#b19cd9'
 
 class ValueSensor( DummyOperator ):
   ui_color = '#CDEB8B'
-
-class JIDOperator( BashOperator ):
-  ui_color = '#006699'
 
 dag_name = os.path.splitext(os.path.basename(__file__))[0]
 
@@ -51,202 +36,33 @@ dag = DAG(
 
 ##### FILESENSORS
 
-fasta_file = FileSensor( task_id='fasta_file',
-    bash_command="""
-{% set filepath = params.base_path + '/' + params.exp_path + '/' + params.file %}
-ls "{{ filepath }}" && [[ -f "{{ filepath }}" ]] && exit 0
-""",
-    params={
-      'base_path': base_path,
-      'exp_path': exp_path,
-      'file': fasta_filepath },
-    dag=dag,
-  )
-
 stream_file = FileSensor( task_id='stream_file',
-    bash_command="""
-{% set filepath = params.base_path + '/' + params.exp_path + '/' + params.file %}
-ls "{{ filepath }}" && [[ -f "{{ filepath }}" ]] && exit 0
-""",
-    params={
-      'base_path': base_path,
-      'exp_path': exp_path,
-      'file': stream_filepath },
-    dag=dag,
-  )
-
-mtz_file = FileSensor( task_id='mtz_file',
-    bash_command="""
-{% set filepath = params.base_path + '/' + params.exp_path + '/' + params.file %}
-ls "{{ filepath }}" && [[ -f "{{ filepath }}" ]] && exit 0
-""",
-    params={
-      'base_path': base_path,
-      'exp_path': exp_path,
-      'file': mtz_filepath },
-    dag=dag,
-  )
-
-map_file = FileSensor( task_id='map_file',
-    bash_command="""
-{% set filepath = params.base_path + '/' + params.exp_path + '/' + params.file %}
-ls "{{ filepath }}" && [[ -f "{{ filepath }}" ]] && exit 0
-""",
-    params={
-      'base_path': base_path,
-      'exp_path': exp_path,
-      'file': map_filepath },
-    dag=dag,
-  )
-
-pdb_file = FileSensor( task_id='pdb_file',
-    bash_command="""
-{% set filepath = params.base_path + '/' + params.exp_path + '/' + params.file %}
-ls "{{ filepath }}" && [[ -f "{{ filepath }}" ]] && exit 0
-""",
-    params={
-      'base_path': base_path,
-      'exp_path': exp_path,
-      'file': pdb_filepath },
-    dag=dag,
-  )
-
-nb_file = FileSensor( task_id='nb_file',
-    bash_command="""
-{% set filepath = params.base_path + '/' + params.exp_path + '/' + params.file %}
-ls "{{ filepath }}" && [[ -f "{{ filepath }}" ]] && exit 0
-""",
-    params={
-      'base_path': base_path,
-      'exp_path': exp_path,
-      'file': nb_filepath },
-    dag=dag,
-  )
-
-nb_published = FileSensor( task_id='nb_published',
-    bash_command="""
-{% set filepath = params.base_path + '/' + params.exp_path + '/' + params.file %}
-ls "{{ filepath }}" && [[ -f "{{ filepath }}" ]] && exit 0
-""",
-    params={
-      'base_path': base_path,
-      'exp_path': exp_path,
-      'file': nb_publishedpath },
+    bash_command="""exit 0""",
     dag=dag,
   )
 
 ##### PROCESS DEFINITIONS
 
+docker_build = JIDOperator( task_id='docker_build',
+    experiment='abcd',
+    run=12,
+    executable="/global/cfs/cdirs/lcls/fpoitevi/autosfx-workflow/scripts/docker_setup/nersc/build_docker_crystfel.slurm",
+    parameters='',
+    dag=dag,
+  )
+
 merging = JIDOperator( task_id='merging',
-    bash_command="""
-cd {{ params.base_path }}/{{ params.exp_path }}
-bash stream2mtz-test.sh
-""",
-    params={
-      'base_path': base_path,
-      'exp_path': exp_path
-    },
+    experiment='abcd',
+    run=12,
+    executable="/global/cfs/cdirs/lcls/fpoitevi/autosfx-workflow/scripts/merging/stream2mtz.slurm",
+    parameters='',
     dag=dag,
   )
-
-phasing = JIDOperator( task_id='phasing',
-    bash_command="""
-cd {{ params.base_path }}/{{ params.exp_path }}
-bash phasing.sh
-""",
-    params={
-      'base_path': base_path,
-      'exp_path': exp_path
-    },
-    dag=dag,
-  )
-
-nb_create = JIDOperator( task_id='nb_create',
-    bash_command="""
-cd {{ params.base_path }}/{{ params.exp_path }}
-bash nb_create.sh
-""",
-    params={
-      'base_path': base_path,
-      'exp_path': exp_path
-    },
-    dag=dag,
-  )
-
-nb_publish = JIDOperator( task_id='nb_publish',
-    bash_command="""
-cd {{ params.base_path }}/{{ params.exp_path }}
-bash nb_publish.sh
-""",
-    params={
-      'base_path': base_path,
-      'exp_path': exp_path
-    },
-    dag=dag,
-  )
-
-elogs_results = JIDOperator( task_id='elogs_results',
-    bash_command="""
-cd {{ params.base_path }}/{{ params.exp_path }}
-bash elogs_results.sh
-""",
-    params={
-      'base_path': base_path,
-      'exp_path': exp_path
-    },
-    dag=dag,
-  )
-
 
 #### DRAW THE DAG
 
-stream_file >> merging >> mtz_file >> phasing >> map_file >> nb_create
+docker_build >> merging
 
-fasta_file >> phasing >> pdb_file >> nb_create >> nb_file >> nb_publish >> nb_published
+stream_file >> merging 
 
-nb_published >> elogs_results
-
-map_file >> elogs_results
-
-pdb_file >> elogs_results
-
-
-
-##### BELOW IS CODE TO BE RECYCLED
-
-  #def _setup( **kwargs ):
-  #  print( kwargs['dag_run'].run_id )
-  #
-  #setup = PythonOperator( task_id='setup',
-  #  python_callable=_setup,
-  #  provide_context=True
-  #)
-  #fasta_file      = FileSensor( task_id='fasta_file' )
-  #runtag_file     = FileSensor( task_id='runtag_file' )
-  #streamlist_file = FileSensor( task_id='streamlist_file' )
-  #mtz_file        = FileSensor( task_id='mtz_file' )
-  #map_file        = FileSensor( task_id='map_file' )
-  #nb_file         = FileSensor( task_id='nb_file' )
-  #consensus_cell = JIDOperator( task_id='consensus_cell' )
-  #consensus_cell = SubDagOperator(
-  #        task_id='consensus_cell',
-  #        subdag=concensus_cell_subdag( os.path.splitext(os.path.basename(__file__))[0], 'consensus_cell'),
-  #        dag=dag,
-  #    )
-  #merging = JIDOperator( task_id='merging' )
-  #def _merging( **kwargs ):
-  #  print('/gpfs/slac/cryo/fs1/daq/lcls/dev/airflow/dags/data/cxic0415/stream2mtz.sh')
-  #merging = BashOperator( 
-  #  task_id='merging',
-  #  bash_command=_merging,
-  #  dag=dag,
-  #  )
-  #phasing = JIDOperator( task_id='phasing' )
-  #create_nb = JIDOperator( task_id='create_nb' )
-  #publish_nb = JIDOperator( task_id='publish_nb' )
-  #elog_diagnosis = JIDOperator( task_id='elog_diagnosis' )
-  #elog_results   = JIDOperator( task_id='elog_results' )
-
-  #runtag_file >> consensus_cell >> elog_diagnosis
-  #consensus_cell >> streamlist_file >> merging 
 
